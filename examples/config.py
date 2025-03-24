4b01ef0f79c0771cb8e169a285ad0821e4cac0d3
JB <jasbirsinghkamboj@gmail.com>
2025-03-24 16:58:04 +0530

    Updated config.py

--- examples/config.py
+++ examples/config.py
@@ -1,8 +1,8 @@
 import os
 from agents import set_default_openai_client, AsyncOpenAI
 
-api_key = os.getenv("API_KEY")  # Fetch the key from an env variable named API_KEY
-base_url = "http://localhost:8080/v1"
+api_key = os.getenv("OPENAI_API_KEY") or "" #Either set API_KEY in environment variable or put it directly here.
+base_url = os.getenv("OPEN_RESPONSES_URL") or "http://localhost:8080/v1" #Either set OPEN_RESPONSES_URL in environment variable or put it directly here.
 
 set_default_openai_client(
     AsyncOpenAI(api_key=api_key, base_url=base_url)
